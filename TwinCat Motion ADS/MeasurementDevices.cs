c99eecf3f08a9a782a311cbf2ab9500ce4633cb6
Cooper <simon.cooper@stfc.ac.uk>
2021-11-09 08:23:21 +0000

    Adding devices list and subwindows for settings

---
+++ TwinCat Motion ADS/MeasurementDevices.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinCat_Motion_ADS
+{
+    public class MeasurementDevices
+    {
+        public List<MeasurementDevice> MeasurementDeviceList { get; set; }
+
+        public MeasurementDevices()
+        {
+            MeasurementDeviceList = new();
+        }
+
+        public int NumberOfDevices
+        {
+            get { return MeasurementDeviceList.Count; }
+        }
+
+
+        public void ClearList()
+        {
+            MeasurementDeviceList.Clear();
+        }
+
+        public void AddDevice(string deviceType)
+        {
+            MeasurementDevice newDevice = new MeasurementDevice(deviceType);
+            MeasurementDeviceList.Add(newDevice);
+        }
+
+        public void RemoveDevice(int i)
+        {
+            if(i> NumberOfDevices)
+            {
+                return;
+            }
+            if (MeasurementDeviceList[i].Connected)
+            {
+                MeasurementDeviceList[i].DisconnectFromDevice();
+            }
+            MeasurementDeviceList.Remove(MeasurementDeviceList[i]);
+            return;
+        }
+
+        public string GetMeasurements()
+        {
+            string measurementString = "";
+            for(int i=0; i<MeasurementDeviceList.Count; i++)
+            {
+                if(MeasurementDeviceList[i].Connected)
+                {
+                    measurementString += "Device " + i + ":" + MeasurementDeviceList[i].DeviceTypeString + ": " + MeasurementDeviceList[i].GetMeasurement() + " ";
+                }
+            }
+            return measurementString;
+        }       
+    }
+}
