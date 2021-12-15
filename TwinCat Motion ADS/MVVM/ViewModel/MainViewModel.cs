478c3f93ec1be244501e9a5c13bc16265a380adf
SCooper <simon.cooper@stfc.ac.uk>
2021-12-15 14:30:46 +0000

    New measurement devices for accessing any variable on the controller and including a timestamp

--- TwinCat Motion ADS/MVVM/ViewModel/MainViewModel.cs
+++ TwinCat Motion ADS/MVVM/ViewModel/MainViewModel.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwinCat_Motion_ADS.Core;
-using Simple.Wpf.Terminal;
 namespace TwinCat_Motion_ADS.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
