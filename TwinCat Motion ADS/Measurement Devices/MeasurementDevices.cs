e94151bcb8a720395435fe23766a71e11cf5fb50
SCooper <simon.cooper@stfc.ac.uk>
2021-12-17 15:05:01 +0000

    XML read/write methods added to allow exporting and importing of all currently configured measurement devices. Will attempt to automatically connect and update channels

--- TwinCat Motion ADS/Measurement Devices/MeasurementDevices.cs
+++ TwinCat Motion ADS/Measurement Devices/MeasurementDevices.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Xml;
+using System.Windows;
 
 namespace TwinCat_Motion_ADS
 {
     [SettingsSerializeAs(SettingsSerializeAs.Xml)]
     public class MeasurementDevices
     {//TwinCat_Motion_ADS.MeasurementDevices
+        MainWindow windowData;
         public List<MeasurementDevice> MeasurementDeviceList { get; set; }
 
         public MeasurementDevices()
         {
+            windowData = (MainWindow)Application.Current.MainWindow;
             MeasurementDeviceList = new();
         }
 
@@ -59,6 +63,237 @@ namespace TwinCat_Motion_ADS
                 }
             }
             return measurementString;
-        }       
+        }      
+        
+        public int ImportDevicesXML(string filepath)
+        {
+            //string filepath = @"C:\Users\SCoop - Work\Documents\Ads Tests\motionchannelTest\testxml.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filepath);
+            XmlNodeList devices = xmlDoc.SelectNodes("MeasurementDevices/MeasurementDevice");
+
+            int deviceCounter = MeasurementDeviceList.Count;
+            foreach(XmlNode device in devices)  //for every device in the measurement devices
+            {
+
+                string DeviceType = device.SelectSingleNode("DeviceType").InnerText;
+                AddDevice(DeviceType);
+                MeasurementDeviceList[deviceCounter].Name = device.SelectSingleNode("Name").InnerText;
+                
+                switch(DeviceType)
+                {
+                    case "DigimaticIndicator":
+                        Console.WriteLine("Importing DigimaticIndicator");
+                        //Comm port
+                        MeasurementDeviceList[deviceCounter].PortName = device.SelectSingleNode("Port").InnerText;
+                        //Baud Rate
+                        MeasurementDeviceList[deviceCounter].UpdateBaudRate(device.SelectSingleNode("BaudRate").InnerText);
+                        break;
+
+                    case "KeyenceTM3000":
+                        Console.WriteLine("Importing KeyenceTm3000");
+                        //Comm port
+                        MeasurementDeviceList[deviceCounter].PortName = device.SelectSingleNode("Port").InnerText;
+                        //Baud Rate
+                        MeasurementDeviceList[deviceCounter].UpdateBaudRate(device.SelectSingleNode("BaudRate").InnerText);
+
+                        //Not to setup channel names and connections
+                        XmlNodeList chSettings = device.SelectNodes("Channel");
+                        Console.WriteLine("Keyence channels : "+chSettings.Count);
+
+                        int i = 0; //channel counter
+                        foreach(XmlNode ch in chSettings)
+                        {
+                            if (i >= MeasurementDeviceList[deviceCounter].keyence.KEYENCE_MAX_CHANNELS)
+                            {
+                                break;
+                            }
+                            MeasurementDeviceList[deviceCounter].keyence.ChName[i] = ch.SelectSingleNode("Name").InnerText;
+                            if (ch.SelectSingleNode("Connected").InnerText == "True")
+                            {
+                                MeasurementDeviceList[deviceCounter].keyence.ChConnected[i] = true;
+                            }
+                            else
+                            {
+                                MeasurementDeviceList[deviceCounter].keyence.ChConnected[i] = false;
+                            }
+                            i++;
+                        }
+                        break;
+
+                    case "Beckhoff":
+                        Console.WriteLine("Importing Beckhoff");
+                        //AMS Net ID
+                        MeasurementDeviceList[deviceCounter].AmsNetId = device.SelectSingleNode("AmsNetID").InnerText;
+
+                        //Channle Types
+                        XmlNodeList digChannels = device.SelectNodes("DigChannel");
+                        XmlNodeList pt100Channels = device.SelectNodes("PT100Channel");
+                        
+                        int chCounter = 0;                       
+                        foreach(XmlNode ch in digChannels)
+                        {
+                            if(chCounter>=MeasurementDeviceList[deviceCounter].beckhoff.DIGITAL_INPUT_CHANNELS)
+                            {
+                                break;
+                            }
+                            if (ch.SelectSingleNode("Connected").InnerText == "True")
+                            {
+                                MeasurementDeviceList[deviceCounter].beckhoff.DigitalInputConnected[chCounter] = true;
+                            }
+                            else
+                            {
+                                MeasurementDeviceList[deviceCounter].beckhoff.DigitalInputConnected[chCounter] = false;
+                            }
+                            chCounter++;
+                        }
+                        chCounter = 0;
+                        foreach (XmlNode ch in pt100Channels)
+                        {
+                            if (chCounter >= MeasurementDeviceList[deviceCounter].beckhoff.PT100_CHANNELS)
+                            {
+                                break;
+                            }
+                            if (ch.SelectSingleNode("Connected").InnerText == "True")
+                            {
+                                MeasurementDeviceList[deviceCounter].beckhoff.PT100Connected[chCounter] = true;
+                            }
+                            else
+                            {
+                                MeasurementDeviceList[deviceCounter].beckhoff.PT100Connected[chCounter] = false;
+                            }
+                            chCounter++;
+                        }
+                        break;
+
+                    case "MotionChannel":
+                        Console.WriteLine("Importing Motion Channel");
+                        MeasurementDeviceList[deviceCounter].motionChannel.VariableType = device.SelectSingleNode("VariableType").InnerText;
+                        MeasurementDeviceList[deviceCounter].motionChannel.VariableString = device.SelectSingleNode("VariablePath").InnerText;
+                        break;
+
+                    case "Timestamp":
+                        break;
+
+                    default:
+                        break;
+                }
+                MeasurementDeviceList[deviceCounter].ConnectToDevice();
+                MeasurementDeviceList[deviceCounter].UpdateChannelList();
+
+                deviceCounter ++;
+            }
+            return devices.Count;
+            
+        }
+
+        public void ExportDeviceesXml(string selectedFile)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNode rootNode = xmlDoc.CreateElement("MeasurementDevices");
+            xmlDoc.AppendChild(rootNode);   //Root of the xml
+
+            //Now create settings for each device
+
+            foreach(MeasurementDevice md in MeasurementDeviceList)
+            {
+                XmlNode deviceNode = xmlDoc.CreateElement("MeasurementDevice");
+                XmlNode nameNode = xmlDoc.CreateElement("Name");
+                XmlNode deviceTypeNode = xmlDoc.CreateElement("DeviceType");
+                
+                //Common setup to all types
+                nameNode.InnerText = md.Name;
+                deviceTypeNode.InnerText = md.DeviceTypeString;
+                deviceNode.AppendChild(nameNode);
+                deviceNode.AppendChild(deviceTypeNode);
+                rootNode.AppendChild(deviceNode);
+
+                //Unique settings
+                switch(md.DeviceTypeString)
+                {
+                    case "DigimaticIndicator":
+                        XmlNode commNode = xmlDoc.CreateElement("Port");
+                        commNode.InnerText = md.PortName;
+                        XmlNode baudNode = xmlDoc.CreateElement("BaudRate");
+                        baudNode.InnerText = md.BaudRate;
+                        deviceNode.AppendChild(commNode);
+                        deviceNode.AppendChild(baudNode);
+                        break;
+
+                    case "KeyenceTM3000":
+                        commNode = xmlDoc.CreateElement("Port");
+                        commNode.InnerText = md.PortName;
+                        baudNode = xmlDoc.CreateElement("BaudRate");
+                        baudNode.InnerText = md.BaudRate;
+                        deviceNode.AppendChild(commNode);
+                        deviceNode.AppendChild(baudNode);
+
+                        for (int i = 0; i<md.keyence.KEYENCE_MAX_CHANNELS;i++)
+                        {
+                            XmlNode channelNode = xmlDoc.CreateElement("Channel");
+                            XmlNode channelName = xmlDoc.CreateElement("Name");
+                            XmlNode channelConnection = xmlDoc.CreateElement("Connected");
+
+                            channelName.InnerText = md.keyence.ChName[i];
+                            channelConnection.InnerText = md.keyence.ChConnected[i].ToString();
+                            channelNode.AppendChild(channelName);
+                            channelNode.AppendChild(channelConnection);
+                            deviceNode.AppendChild(channelNode);
+                        }
+                        break;
+
+                    case "Beckhoff":
+                        XmlNode amsNode = xmlDoc.CreateElement("AmsNetID");
+                        amsNode.InnerText = md.AmsNetId;
+                        deviceNode.AppendChild(amsNode);
+
+                        //Create digital channles
+                        for(int i=0;i<md.beckhoff.DIGITAL_INPUT_CHANNELS;i++)
+                        {
+                            XmlNode channelNode = xmlDoc.CreateElement("DigChannel");
+                            XmlNode channelConnection = xmlDoc.CreateElement("Connected");
+                            channelConnection.InnerText = md.beckhoff.DigitalInputConnected[i].ToString();
+                            channelNode.AppendChild(channelConnection);
+                            deviceNode.AppendChild(channelNode);
+                        }
+                        //create pt100 channels
+                        for (int i = 0; i < md.beckhoff.PT100_CHANNELS; i++)
+                        {
+                            XmlNode channelNode = xmlDoc.CreateElement("PT100Channel");
+                            XmlNode channelConnection = xmlDoc.CreateElement("Connected");
+                            channelConnection.InnerText = md.beckhoff.PT100Connected[i].ToString();
+                            channelNode.AppendChild(channelConnection);
+                            deviceNode.AppendChild(channelNode);
+                        }
+                        break;
+
+                    case "MotionChannel":
+                        XmlNode varTypeNode = xmlDoc.CreateElement("VariableType");
+                        XmlNode varPathNode = xmlDoc.CreateElement("VariablePath");
+                        varTypeNode.InnerText = md.motionChannel.VariableType;
+                        varPathNode.InnerText = md.motionChannel.VariableString;
+                        deviceNode.AppendChild(varTypeNode);
+                        deviceNode.AppendChild(varPathNode);
+                        break;
+
+                    case "Timestamp":
+                        //No settings to export
+                        break;
+
+                }
+
+
+            }
+
+            
+            
+
+            
+            
+
+
+            xmlDoc.Save(selectedFile);
+        }
+
     }
 }
