94d0aa994f0e82721b6d4fc618ad07970e990e1a
Cooper <simon.cooper@stfc.ac.uk>
2022-05-25 10:24:05 +0100

    Tidying up UI elements

---
+++ TwinCat Motion ADS/APPLICATION WINDOWS/User Controls/SettingControlMainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Controls;
+
+
+namespace TwinCat_Motion_ADS
+{
+    /// <summary>
+    /// Interaction logic for SettingControl.xaml
+    /// </summary>
+    public partial class SettingControlMainWindow : UserControl
+    {
+        public SettingControlMainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        public new string SetValue { get; set; }
+        public string SetName { get; set; }
+        public int BoxWidth { get; set; } = 200;
+        public string strTests { get; set; }
+    }
+}
---
+++ TwinCat Motion ADS/ListBoxWriter.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TwinCat_Motion_ADS
+{
+    public class ListBoxStatusItem
+    {
+        public string timestamp { get; set; }
+        public string statusMessage { get; set; }
+        public ListBoxStatusItem(string status)
+        {
+            statusMessage = status;
+            timestamp = DateTime.Now.ToString();
+        }
+    }
+
+    public class ListBoxWriter : TextWriter
+    {
+        private ListBox list;
+        private StringBuilder content = new StringBuilder();
+
+        public ListBoxWriter(ListBox list)
+        {
+            this.list = list;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            content.Append(value);
+            if (value == '\n')
+            {
+                ListBoxStatusItem temp = new(content.ToString());
+                //((ObservableCollection<string>)(list.ItemsSource)).Add(content.ToString());
+                ((ObservableCollection<ListBoxStatusItem>)(list.ItemsSource)).Add(temp);
+                //list.Items.Add(content.ToString());
+                content = new StringBuilder();
+            }
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.UTF8; }
+        }
+    }
+
+    class ListBoxBehavior
+    {
+        static readonly Dictionary<ListBox, Capture> Associations =
+               new Dictionary<ListBox, Capture>();
+
+        public static bool GetScrollOnNewItem(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ScrollOnNewItemProperty);
+        }
+
+        public static void SetScrollOnNewItem(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ScrollOnNewItemProperty, value);
+        }
+
+        public static readonly DependencyProperty ScrollOnNewItemProperty =
+            DependencyProperty.RegisterAttached(
+                "ScrollOnNewItem",
+                typeof(bool),
+                typeof(ListBoxBehavior),
+                new UIPropertyMetadata(false, OnScrollOnNewItemChanged));
+
+        public static void OnScrollOnNewItemChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var listBox = d as ListBox;
+            if (listBox == null) return;
+            bool oldValue = (bool)e.OldValue, newValue = (bool)e.NewValue;
+            if (newValue == oldValue) return;
+            if (newValue)
+            {
+                listBox.Loaded += ListBox_Loaded;
+                listBox.Unloaded += ListBox_Unloaded;
+                var itemsSourcePropertyDescriptor = TypeDescriptor.GetProperties(listBox)["ItemsSource"];
+                itemsSourcePropertyDescriptor.AddValueChanged(listBox, ListBox_ItemsSourceChanged);
+            }
+            else
+            {
+                listBox.Loaded -= ListBox_Loaded;
+                listBox.Unloaded -= ListBox_Unloaded;
+                if (Associations.ContainsKey(listBox))
+                    Associations[listBox].Dispose();
+                var itemsSourcePropertyDescriptor = TypeDescriptor.GetProperties(listBox)["ItemsSource"];
+                itemsSourcePropertyDescriptor.RemoveValueChanged(listBox, ListBox_ItemsSourceChanged);
+            }
+        }
+
+        private static void ListBox_ItemsSourceChanged(object sender, EventArgs e)
+        {
+            var listBox = (ListBox)sender;
+            if (Associations.ContainsKey(listBox))
+                Associations[listBox].Dispose();
+            Associations[listBox] = new Capture(listBox);
+        }
+
+        static void ListBox_Unloaded(object sender, RoutedEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+            if (Associations.ContainsKey(listBox))
+                Associations[listBox].Dispose();
+            listBox.Unloaded -= ListBox_Unloaded;
+        }
+
+        static void ListBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+            var incc = listBox.Items as INotifyCollectionChanged;
+            if (incc == null) return;
+            listBox.Loaded -= ListBox_Loaded;
+            Associations[listBox] = new Capture(listBox);
+        }
+
+        class Capture : IDisposable
+        {
+            private readonly ListBox listBox;
+            private readonly INotifyCollectionChanged incc;
+
+            public Capture(ListBox listBox)
+            {
+                this.listBox = listBox;
+                incc = listBox.ItemsSource as INotifyCollectionChanged;
+                if (incc != null)
+                {
+                    incc.CollectionChanged += incc_CollectionChanged;
+                }
+            }
+
+            void incc_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    listBox.ScrollIntoView(e.NewItems[0]);
+                    listBox.SelectedItem = e.NewItems[0];
+                }
+            }
+
+            public void Dispose()
+            {
+                if (incc != null)
+                    incc.CollectionChanged -= incc_CollectionChanged;
+            }
+        }
+    }
+}
